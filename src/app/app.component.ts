08e936deafbaf44f394ae89c2b8ae6b19ed9d9d9
phisonkha <phison.khankhang@allianz.com>
2020-07-20 15:02:44 +0700

    input firstname lastname code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello';
+  title = 'phisonk';
+  score = 100;
+  show = true;
+  firstName = '';
+  lastName = '';
+  name = '';
+  colors = ['Red','Green','Blue']; //Array
+  inputYourName(event : any): void{
+    this.firstName = event.target.value;
+  }
+  inputLastname(input: string):void{
+    this.lastName = input;
+  }
+  onClickSave(){
+    this.name = this.firstName + ' ' + this.lastName;
+  }
+  onEnter(){
+    if(this.firstName != '' && this.lastName != ''){
+      this.onClickSave();
+    }
+    else{
+      alert("Please Fill All Input Box");
+    }
+  }
+  
 }
